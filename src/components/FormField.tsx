9be194913f19658b53a5342de09d76179b6044a8
christophblessing <christophblessing@users.noreply.github.com>
2020-10-07 15:46:55 +0200

    adds description and id prop to FormField

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -2,25 +2,27 @@ import React, { ChangeEvent } from "react";
 
 interface Props {
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  id: string;
   type: string;
   name: string;
   label: string;
+  description?: string;
   value: string | number;
 }
 
 function FormField(props: Props) {
-  const id = props.name + Math.random() * 100;
   return (
     <div className="form-group">
-      <label htmlFor={id}>{props.label}</label>
+      <label htmlFor={props.id}>{props.label}</label>
       <input
         className="form-control"
         type={props.type}
         name={props.name}
-        id={id}
+        id={props.id}
         value={props.value}
         onChange={props.onChange}
       />
+      {props.description && <small>{props.description}</small>}
     </div>
   );
 }
